058da66c3db0a752b3f4e79f720c6232002f0190
Gabriel Castro <gabrielcssg@gmail.com>
2021-07-29 22:51:59 -0300

    estilizacao dos cards de planos

---
+++ resources/views/home.blade.php
@@ -0,0 +1,203 @@
+@extends('layout.base')
+
+<head>
+  <!-- Styles -->
+  <link href="{{ asset('css/home.css') }}" rel="stylesheet">
+</head>
+
+@section('content')
+<header id="highlight">
+  <div class="header-content">
+    <div class="header-info">
+      <h1>HOSPEDAGEM DE SITES</h1>
+      <p class="header-sub">
+        Tenha site e emails a partir
+        de R$ 4,50 /mês
+      </p>
+      <p class="header-text">
+        Mussum Ipsum é simplesmente uma
+        simulação de texto da indústria tipográfica e
+        de impressos, e vem sendo utilizado desde
+        o século XVI.
+      </p>
+    </div>
+  </div>
+</header>
+
+<section id="plans" class="plans-container">
+  <div class="plans-title">
+    Conheça os planos da <strong>Matilha.<span class="red">host</span></strong>
+  </div>
+  <div class="plans-content">
+    <div class="plan-card bg-dark">
+      <img src="img/img_dog_1.png" alt="plano pug">
+      <span class="title-card">Pug</span>
+      <span>Solução ideal para iniciantes</span>
+      <div class="separator"></div>
+      <span class="card-price">R$ 4,99 /mês</span>
+      <ul>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_bloqueado.svg" alt="bloqueado" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_bloqueado.svg" alt="bloqueado" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_bloqueado.svg" alt="bloqueado" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_bloqueado.svg" alt="bloqueado" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_bloqueado.svg" alt="bloqueado" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_bloqueado.svg" alt="bloqueado" /> Mussum Ipsum
+        </li>
+      </ul>
+      <div class="buy-btn">
+        <span>Comprar</span>
+      </div>
+    </div>
+    <div class="plan-card bg-dark">
+      <img src="img/img_dog_2.png" alt="plano buldogue">
+      <span class="title-card">Pug</span>
+      <span>Solução ideal para medianos</span>
+      <div class="separator"></div>
+      <span class="card-price">R$ 4,99 /mês</span>
+      <ul>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_bloqueado.svg" alt="bloqueado" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_bloqueado.svg" alt="bloqueado" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_bloqueado.svg" alt="bloqueado" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_bloqueado.svg" alt="bloqueado" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_bloqueado.svg" alt="bloqueado" /> Mussum Ipsum
+        </li>
+      </ul>
+      <div class="buy-btn">
+        <span>Comprar</span>
+      </div>
+    </div>
+    <div class="plan-card bg-dark">
+      <img src="img/img_dog_3.png" alt="plano doberman">
+      <span class="title-card">Pug</span>
+      <span>Solução ideal para avançados</span>
+      <div class="separator"></div>
+      <span class="card-price">R$ 4,99 /mês</span>
+      <ul>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check"  /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_bloqueado.svg" alt="bloqueado" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_bloqueado.svg" alt="bloqueado" /> Mussum Ipsum
+        </li>
+      </ul>
+      <div class="buy-btn">
+        <span>Comprar</span>
+      </div>
+    </div>
+    <div class="plan-card bg-dark">
+      <img src="img/img_dog_4.png" alt="plano pitbull">
+      <span class="title-card">Pug</span>
+      <span>Solução ideal para experientes</span>
+      <div class="separator"></div>
+      <span class="card-price">R$ 4,99 /mês</span>
+      <ul>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+        <li>
+          <img src="img/icon_aprovado.svg" alt="check" /> Mussum Ipsum
+        </li>
+      </ul>
+      <div class="buy-btn">
+        <span>Comprar</span>
+      </div>
+    </div>
+  </div>
+</section>
+
+@endsection
